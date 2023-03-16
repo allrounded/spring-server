7049d08c7bfa2c6e3e09ac920fc84f548e2e8d1a
nathan29849 <phs960418@gmail.com>
2023-03-16 23:22:49 +0900

    feat: [#2] init project

---
+++ mogong/src/main/java/hufs/team/mogong/MogongApplication.java
@@ -0,0 +1,13 @@
+package hufs.team.mogong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogongApplication.class, args);
+	}
+
+}
