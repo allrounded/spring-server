34d26a6bdcaf034a08291ea3541ec5a41f06f55c
nathan29849 <phs960418@gmail.com>
2023-06-08 01:02:26 +0900

    API DOCS 최신화

--- mogong/src/test/java/hufs/team/mogong/MogongApplicationTests.java
+++ mogong/src/test/java/hufs/team/mogong/MogongApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@Disabled
+//@Disabled
 @SpringBootTest
 @ActiveProfiles("test")
 class MogongApplicationTests {
